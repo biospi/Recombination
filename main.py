13be176db1c324b15c82e2c83ff212e52e8f7380
Axel <axel.montout@bristol.ac.uk>
2024-07-17 15:28:20 +0100

    added cleasn up function for raw data.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import typer
 import warnings
 
-from utils import count_isolates_in_tar_gz, run_cmd
+from utils import count_isolates_in_tar_gz, filter_isolates_in_tar_gz, run_cmd
 warnings.filterwarnings("ignore")
 import pandas as pd
 
@@ -11,7 +11,8 @@ def main(
     dataset_filepath: Path = typer.Option(
         ..., exists=True, file_okay=True, dir_okay=False, resolve_path=True
     ),
-    output_dir: str = "output"
+    output_dir: str = "output",
+    min_length: int = 1000
 ):
     output_dir = Path(output_dir)
     output_dir.mkdir(parents=True, exist_ok=True)
@@ -20,11 +21,18 @@ def main(
     iso_count = count_isolates_in_tar_gz(dataset_filepath.as_posix())
     print(f"Found {iso_count} isolates in {dataset_filepath}.")
 
-    filepath_vcf = Path(f"{dataset_filepath.name}.vcf")
+    cleaned_tar_gz_filepath = dataset_filepath.parent / "cleaned-wgs-mapping.tar.gz"
+
+    filter_isolates_in_tar_gz(dataset_filepath.as_posix(), cleaned_tar_gz_filepath.as_posix(), min_length)
+    
+    iso_count = count_isolates_in_tar_gz(cleaned_tar_gz_filepath.as_posix())
+    print(f"Found {iso_count} isolates in {cleaned_tar_gz_filepath}.")
+
+    filepath_vcf = Path(f"{cleaned_tar_gz_filepath.name}.vcf")
     print(filepath_vcf)
     print("building snp file...")
     run_cmd(
-        f"snp-sites -o {filepath_vcf.as_posix()} -v {dataset_filepath.as_posix()}",
+        f"snp-sites -o {filepath_vcf.as_posix()} -v {cleaned_tar_gz_filepath.as_posix()}",
         output_dir,
         "vcf",
         iso_count,
--- utils.py
+++ utils.py
@@ -6,9 +6,45 @@ from Bio import SeqIO
 import pandas as pd
 import tarfile
 import gzip
-from io import TextIOWrapper
+from io import TextIOWrapper, BytesIO
 from Bio import SeqIO
 from pathlib import Path
+
+
+def filter_isolates_in_tar_gz(tar_gz_filepath, cleaned_tar_gz_filepath, min_length):
+    length_list =[]
+    with tarfile.open(tar_gz_filepath, "r:gz") as tar, tarfile.open(cleaned_tar_gz_filepath, "w:gz") as cleaned_tar:
+        for member in tar.getmembers():
+            if member.isfile() and member.name.endswith('.fasta'):
+                file = tar.extractfile(member)
+                if file is not None:
+                    with TextIOWrapper(file, encoding='utf-8') as fasta_file:
+                        #sequences = [record for record in SeqIO.parse(fasta_file, "fasta") if len(record.seq) >= min_length]
+
+                        sequences = []
+                        for record in SeqIO.parse(fasta_file, "fasta"):
+                            length_list.append(len(record.seq))
+                            if len(record.seq) <= min_length:
+                                continue    
+                            sequences.append(record)
+
+                        if sequences:
+                            # Create a temporary file to write filtered sequences
+                            tmp_file = BytesIO()
+                            SeqIO.write(sequences, tmp_file, "fasta")
+                            tmp_file.seek(0)
+                            # Create a new tarinfo object for the filtered sequences file
+                            tarinfo = tarfile.TarInfo(name=member.name)
+                            tarinfo.size = len(tmp_file.getvalue())
+                            # Add the filtered sequences file to the new tar.gz archive
+                            cleaned_tar.addfile(tarinfo, fileobj=tmp_file)
+
+    # Create a DataFrame from the lengths list
+    df = pd.DataFrame(length_list, columns=['Length'])
+    length_counts = df['Length'].value_counts().reset_index()
+    length_counts.columns = ['Length', 'Count']
+    lengths_csv_filepath = Path("sequence_lengths.csv")
+    length_counts.to_csv(lengths_csv_filepath, index=False)
 
 
 def count_isolates_in_tar_gz(tar_gz_filepath):
